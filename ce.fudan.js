07d35b0a16069efca8652f2b79b5be41f3ac71ad
CatlessFish <791517865@qq.com>
2023-05-09 18:45:36 +0800

    prepare for github pages

--- ce.fudan.js
+++ ce.fudan.js
@@ -3,10 +3,7 @@ questions.forEach((q, i) => {
     const title = q.children[0].querySelector('div').innerText
     const answerText = q.children[1].querySelector('label').textContent
     const answerOption = q.children[1].querySelector('a')
-    if (i > 16 && i < 20) {
-        return
-    }
     console.log(`${title} 已选择： ${answerText}`)
     answerOption.click()
 })
-alert('请手动完成第18至20题')
+alert('请检查您的选项，根据实际情况修改')
